eb5ed033a8e6eedde314795436d822f43a9f0eee
Songyu Lu <sol009@ucsd.edu>
2021-10-13 13:01:08 -0700

    format data

--- SPI/spi_interface.v
+++ SPI/spi_interface.v
@@ -24,7 +24,7 @@ module spi_state(
 // ignore miso for now
 
 reg [39:0] MOSI;                    // SPI shift register
-reg [7:0] count;                    // control counter
+reg [5:0] count;                    // control counter
 // reg cs_slave;
 reg sclk;
 reg XREADY;
@@ -32,8 +32,9 @@ reg XWAIT;
 reg [2:0] state;
 
 // read data from csv: 2 byte for addr, 1 byte for data
-reg [23:0] data_in [0:367];           
-reg [9:0]  entry = 9'd368;             // 9 bit counter for read file line by line
+reg [23:0] data_in [0:367];       
+reg [23:0] data_cur;    
+reg [9:0]  entry = 9'd368;          // 9 bit counter for read file line by line
 
 initial begin
     
@@ -50,53 +51,64 @@ end
 always@(posedge clk or posedge reset)
     if(reset) begin
         MOSI  <= 40'b0;
-        count <= 7'd40;
+        count <= 5'd24;
         sclk  <=  1'b0;
         XRESET <= cycle_wait;
         XREADY <= 1'b0;
     end
-    else begin
+
+    else begin     
+        /*
+        1: begin
+            // wait 200 before XREADY = 1
+            sclk = 1'b1;
+            if (XRESET) begin
+                XREADY <= 1;
+                XRESET <= cycle_wait;
+                state  <= 2;
+            else begin
+                XRESET <= XRESET - 1;
+            end
+                sclk = 1'b0;
+        end
+
+        2: begin
+            // wait another 200 clock after XREADY = 1
+            sclk <= 1'b1;
+            if(XRESET) begin
+                state <= 3;
+            else begin
+                XRESET = XRESET-1;
+            end
+        end 
+        */
         case(state)
             0: begin
                 sclk   <= 1'b0;
                 state  <= 1;
-            end      
-
+            end 
             1: begin
-                // wait 200 before XREADY = 1
-                sclk = 1'b1;
-                if (XRESET) begin
-                    XREADY <= 1;
-                    XRESET <= cycle_wait;
-                    state  <= 2;
-                else begin
-                    XRESET <= XRESET - 1;
+                sclk        <= 1'b0;
+                data_cur    <= data_in[count-1];
+                count       <= count-1;
+                state       <= 2;
+            end 
+
+            2:begin
+                sclk    <= 1'b1;
+                if(count > 0)begin
+                    state <= 1;
                 end
-                sclk = 1'b0;
+                    
+                else begin
+                    MOSI = {8'd224, data_cur[23:7], 8'd1, data_cur[7:0] };
+                    count <= 24;
+                    state <= 0;
+                end
             end
 
-            2: begin
-                // wait another 200 clock after XREADY = 1
-                sclk <= 1'b1;
-                if(XRESET) begin
-                    state <= 3;
-                else begin
-                    XRESET = XRESET-1;
-                end
-            end 
-
-            3: begin
-                sclk = 1'b1;
-                if (count > 0) begin
-                    MOSI  <= data[count-1];
-                    count <= count - 1;
-                else begin
-                    count <= 7'd40;
-                end
-                sclk = 1'b0;
-            end 
-
             default: state <= 0;
+        
         endcase
     end
 
