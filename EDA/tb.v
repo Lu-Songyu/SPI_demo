cfd33482fb0502c8290e62c55a67a177576e3878
Songyu Lu <57025781+Lu-Songyu@users.noreply.github.com>
2021-10-29 02:10:08 -0700

    Update tb.v

--- EDA/tb.v
+++ EDA/tb.v
@@ -4,12 +4,19 @@
 module tb;
   reg clk;
   reg reset;
-  reg [15:0] data_in;
+  reg [23:0] data_in;
   
   wire spi_sclk;
   wire spi_cs_l;
   wire spi_data;
   wire [5:0] counter;
+  
+  reg [23:0] data_mem [0:366];
+
+  integer i;
+  
+  // MISO word to transmit 1st = 12'b110101110011 or 12'hD73
+  // MISO word to transmit 2nd = 12'b000000000011 or 12'h3
  
   SPI_state_machine uut(
     .clk(clk),
@@ -32,14 +39,23 @@ module tb;
   
   always #5 clk = ~clk;
   
+  initial begin
+    $readmemb("data.mem", data_mem);
+  end
+
   initial
     begin
       
       $dumpfile("dump.vcd"); $dumpvars;
-      #10  data_in = 24'd16777200;
-            reset = 0;
-      #815 data_in = 24'd2000;
-      #815 data_in = 24'd3000;
+      #10 reset = 0;
+      for (i = 0; i < 367; i = i + 1) begin
+        #815 data_in = data_mem[i];
+      end
+      //   data_in = 24'd16777200;
+            
+      // #815 data_in = 24'd2000;
+      // #815 data_in = 24'd3000;
       #815 $finish;
     end
 endmodule
+
